c2485e4b24185d704cac91e718743382f73a3b9e
CrystalNyein <nyeinchanlay99@gmail.com>
2021-10-31 13:06:05 +0630

    mAdd function to accordion & design fix

---
+++ index.js
@@ -0,0 +1,17 @@
+const accordDisplay = (e) => {
+  let parentNode;
+  switch (e.target.nodeName) {
+    case "H3":
+    case "I":
+      parentNode = e.target.parentNode.parentNode;
+      break;
+    case "DIV":
+    case "P":
+      parentNode = e.target.parentNode;
+  }
+  if (parentNode.classList.contains("active")) {
+    parentNode.classList.remove("active");
+  } else {
+    parentNode.classList.add("active");
+  }
+};
