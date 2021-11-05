be91b47423c166af9bb10229d536514486997ab1
CrystalNyein <nyeinchanlay99@gmail.com>
2021-11-05 14:20:55 +0630

    Add transition to FAQ to make it smoother

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const accordDisplay = (e) => {
+  console.log(e.target, e.target.nodeName);
   let parentNode;
   switch (e.target.nodeName) {
     case "H2":
@@ -6,8 +7,13 @@ const accordDisplay = (e) => {
       parentNode = e.target.parentNode.parentNode;
       break;
     case "DIV":
+      if (e.target.classList.contains("accordion"))
+        parentNode = e.target.parentNode;
+      else parentNode = e.target;
+      break;
     case "P":
       parentNode = e.target.parentNode;
+      break;
   }
   if (parentNode.classList.contains("active")) {
     parentNode.classList.remove("active");
